e36ad556a0c85bd9497f0d852475f57ba4688348
Daniel Savio <spnjc137@gmail.com>
2023-08-24 15:35:53 -0300

    Feat: Converting an .icd file in a json

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,4 +1,6 @@
 import { app, BrowserWindow } from 'electron';
 import App from './app';
+import Xml from './xml';
 
 App.main(app, BrowserWindow);
+Xml.xmlParser('./src//archive/scl/Jiga.cid');
---
+++ src/main/xml.ts
@@ -0,0 +1,21 @@
+export default class Xml{
+
+    static xml2js = require('xml2js');
+    static fs = require('fs');
+    static parser = new this.xml2js.Parser({ attrkey: "ATTR" });
+
+    static xmlParser(xmlUrl:string) {
+         let xml_string = Xml.fs.readFileSync(xmlUrl, "utf8");
+         
+         Xml.parser.parseString(xml_string, function(error: any, result: JSON) {
+                if(error === null) {
+                    Xml.fs.writeFileSync("./src/archive/json/Jiga.json", JSON.stringify(result));
+                }
+                else {
+                    console.log(error);
+                }
+            });
+    }
+
+
+} 
